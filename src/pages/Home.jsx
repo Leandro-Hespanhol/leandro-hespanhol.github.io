48a145ffa71449bb550947fe79ec5de0f42b5b01
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-12-22 13:22:42 -0300

    working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Type from '../components/TypeWritter';
 import foto from '../images/leandro-port.jpeg';
 import Header from './Header';
 
@@ -6,15 +7,14 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <div>
           <Header />
-        </div>
         <div className="img-user">
             <h1 className="title-user">
               Hi! I'm Leandro Hespanhol 
             </h1>
-          <img src={foto} alt="Leandro Hespanhol" id="foto" />
+          <img src={foto} alt="Leandro Hespanhol" id="foto" className="port-image" />
         </div>
+          <h1 className="title-user type-writter">{<Type />}</h1>
       </div>
     );
   }
