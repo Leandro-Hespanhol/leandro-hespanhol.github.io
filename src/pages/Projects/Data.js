63974535a9a06d7b3459aed84df21a2e9bf86017
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-12-23 13:04:09 -0300

    lower case

--- src/pages/Projects/Data.js
+++ src/pages/Projects/Data.js
@@ -3,7 +3,7 @@ export const reactProjects = [
     name: 'Star Wars Planet Search' ,
     habilities: 'React, Context API, Hooks.',
     description: 'On this project i worked on a several different filters, using onInputChange filtering, combining different categories and filtering onClick, adding filters over filters changing dropDown options. The effects of removing a filter and getting the object back to the previous state was the most challenging, take a look!',
-    link: 'https://leandro-hespanhol.github.io/starWars-planet-search/',
+    link: 'https://leandro-hespanhol.github.io/star-wars-planet-search/',
   },
   {
     name: 'TrybeWallet' ,
