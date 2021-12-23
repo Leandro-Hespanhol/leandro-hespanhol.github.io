1cc48638524d41f23f113596f5da0cdcd0b8b732
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-12-23 13:21:11 -0300

    fixing links

--- src/pages/Projects/Data.js
+++ src/pages/Projects/Data.js
@@ -3,13 +3,13 @@ export const reactProjects = [
     name: 'Star Wars Planet Search' ,
     habilities: 'React, Context API, Hooks.',
     description: 'On this project i worked on a several different filters, using onInputChange filtering, combining different categories and filtering onClick, adding filters over filters changing dropDown options. The effects of removing a filter and getting the object back to the previous state was the most challenging, take a look!',
-    link: 'https://leandro-hespanhol.github.io/projects/star-wars-planet-search/',
+    link: 'https://leandro-hespanhol.github.io/StarWars-Planet-Search/',
   },
   {
     name: 'TrybeWallet' ,
     habilities: 'React, Redux.',
     description: 'This project is about Redux, handling props on a Store, using reducers and actions to use and modify then, working with dispatches, mapDispatchToProps, mapStateToProps.',
-    link: 'https://leandro-hespanhol.github.io/projects/trybewallet/',
+    link: 'https://leandro-hespanhol.github.io/TrybeWallet/',
   },
   {
     name: 'TrybeTunes' ,
