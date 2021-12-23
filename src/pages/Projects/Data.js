ee9947bd338110a24a4fd826110a13efe83a13d0
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-12-23 13:09:45 -0300

    links

--- src/pages/Projects/Data.js
+++ src/pages/Projects/Data.js
@@ -3,18 +3,18 @@ export const reactProjects = [
     name: 'Star Wars Planet Search' ,
     habilities: 'React, Context API, Hooks.',
     description: 'On this project i worked on a several different filters, using onInputChange filtering, combining different categories and filtering onClick, adding filters over filters changing dropDown options. The effects of removing a filter and getting the object back to the previous state was the most challenging, take a look!',
-    link: 'https://leandro-hespanhol.github.io/star-wars-planet-search/',
+    link: 'https://leandro-hespanhol.github.io/projects/star-wars-planet-search/',
   },
   {
     name: 'TrybeWallet' ,
     habilities: 'React, Redux.',
     description: 'This project is about Redux, handling props on a Store, using reducers and actions to use and modify then, working with dispatches, mapDispatchToProps, mapStateToProps.',
-    link: 'https://leandro-hespanhol.github.io/trybewallet/',
+    link: 'https://leandro-hespanhol.github.io/projects/trybewallet/',
   },
   {
     name: 'TrybeTunes' ,
     habilities: 'React, props, routes',
     description: 'Fetching an API from itunes i was able to search and list preview musics. This project is all about understanding props. It taught me the basic concepts of React, routing with react-router-dom, exploring matchers, was really fun.',
-    link: 'https://leandro-hespanhol.github.io/trybetunes/',
+    link: 'https://leandro-hespanhol.github.io/projects/trybetunes/',
   },
 ]
