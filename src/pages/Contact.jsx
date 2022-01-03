8a808919c57d65ff57db6bb65030ef759732cf1b
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-01-03 18:57:20 -0300

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import Header from './Header'
+import React from 'react';
+import Header from './Header';
+import './Contact.css';
+import githubImage from '../images/github-logo.png'
+import linkedinImage from '../images/linkedin-logo.png'
 
 export default function Contact() {
   return (
     <div>
       <Header />
-      Contact
+      <div className="main-div">
+        <div className="contact-container github">
+          <a href="https://github.com/Leandro-Hespanhol"
+            target="_blank"
+            className="github-image"
+            rel="noreferrer">
+            <img src={ githubImage } alt="Github logo" />
+          </a>
+        </div>
+        <div className="contact-container linkedin">
+          <a href="https://www.linkedin.com/in/leandro-jos%C3%A9-ferreira-hespanhol-91b3a2136/"
+            target="_blank"
+            className="linkedin-image"
+            rel="noreferrer">
+            <img src={ linkedinImage } alt="Linkedin logo" />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
