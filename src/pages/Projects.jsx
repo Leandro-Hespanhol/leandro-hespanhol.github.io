7f06dca19b6d2c4a787f539442d6c93f6f752b33
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-01-06 13:12:35 -0300

    projects css

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
-import Header from './Header'
-import { reactProjects } from './Projects/Data'
-import './Project.css'
+import React from 'react';
+import Header from './Header';
+import { reactProjects } from './Projects/Data';
+import './Project.css';
 
 export default function Projects() {
   return (
     <div>
       <Header />
-        <div className="window-div">
-          {reactProjects.map(({ name, habilities, image, description, link}) => 
-          <div key={ name } className="card-div">
-            <h2>{ name }</h2>
-            <div className="link-img-div">
-              <a href={ link } target="_blank" rel="noreferrer">
-                <img src={ image } alt={ `${name} logo` } className="project-picture" /> 
-              </a> 
+      <div className="window-div">
+        {reactProjects.map(
+          ({ name, abilities, image, description, link, projectLink }) => (
+            <div key={name} className="card-div">
+              <h2>{name}</h2>
+              <div className="link-img-div">
+                <a href={link} target="_blank" rel="noreferrer">
+                  <img
+                    src={image}
+                    alt={`${name} logo`}
+                    className="project-picture"
+                  />
+                </a>
+              </div>
+              <h4>Core habilities: {abilities}</h4>
+              <p>{description}</p>
+              <div className="project-github-links">
+                <a href={link} target="_blank" rel="noreferrer">
+                  <h4 className="button-link">Go to Github</h4>
+                </a>
+                <a href={projectLink} target="_blank" rel="noreferrer">
+                  <h4 className="button-link">Go to Project</h4>
+                </a>
+              </div>
             </div>
-            <h4>Core habilities: { habilities }</h4>
-            <p>{ description }</p>
-          <a href={ link } target="_blank" rel="noreferrer">
-            <h4 className="project-link">{ name }</h4>
-          </a>
-          </div>
-          )}
-        </div>
+          )
+        )}
+      </div>
     </div>
-  )
+  );
 }
