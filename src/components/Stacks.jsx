b8f77274ac3103ca7ab06a9f641d3302baf83562
Leandro Hespanhol <leandrohespanhol@gmail.com>
2023-02-07 23:45:06 -0300

    refact: stacks-loop

--- src/components/Stacks.jsx
+++ src/components/Stacks.jsx
@@ -9,152 +9,21 @@ export default function stacks(props) {
     <p className="intro-description">
       {t('home.stacks')}
     </p>
-      {stacksDocumentation.map(({git, node, npm, mocha, mysql, HTML, CSS, javascript, typescript, react, redux, jest, docker}) => (
     <div className="stacks-container">
+      {stacksDocumentation.map(({documentantion, svg, stack, alt}) => (
       <div className="stack-description-container">
-      <a href={git} target="_blank" rel="noreferrer">
+      <a href={documentantion} target="_blank" rel="noreferrer">
         <img
             align="center"
-            alt="LeoH-Git"
+            alt={alt}
             className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+            src={svg}
           />
       </a>
-        <p>Git</p>
+        <p>{stack}</p>
       </div>
-      <div className="stack-description-container">
-        <a href={node} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-NodeJS"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" 
-          />
-        </a>
-         <p>NodeJS</p>
-      </div>
-      <div className="stack-description-container">
-        <a href={npm} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-npm"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg"
-          />
-        </a>
-         <p>Npm</p>
-      </div>
-      <div className="stack-description-container">
-        <a href={mocha} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-mocha"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg"
-          />
-        </a>
-          <p>Mocha</p>
-      </div>
-      <div className="stack-description-container">
-        <a href={mysql} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-mysql"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
-          />
-        </a>
-        <p>mySQL</p>
-      </div>
-      <div className="stack-description-container">
-      <a href={HTML} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-html"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-          />
-        </a>
-        <p>HTML</p>
-      </div>
-      <div className="stack-description-container">
-      <a href={CSS} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-css"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg"
-          />
-        </a>
-        <p>CSS</p>
-      </div>
-      <div className="stack-description-container">
-      <a href={javascript} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-javascript"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
-          />
-        </a>
-        <p>Javacript</p>
-      </div>
-      <div className="stack-description-container">
-      <a href={typescript} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-typescript"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg"
-          />
-        </a>
-        <p>Typescript</p>
-      </div>
-      <div className="stack-description-container">
-      <a href={react} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-React"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-          />
-        </a>
-        <p>React</p>
-      </div>
-      <div className="stack-description-container">
-      <a href={redux} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-Redux"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
-          />
-        </a>
-        <p>Redux</p>
-      </div>
-      <div className="stack-description-container">
-      <a href={jest} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-jest"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"
-          />
-        </a>
-        <p>Jest</p>
-      </div>
-      <div className="stack-description-container">
-      <a href={docker} target="_blank" rel="noreferrer">
-          <img
-            align="center"
-            alt="LeoH-docker"
-            className="stack-icon"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
-          />
-        </a>
-        <p>Docker</p>
-      </div>
-    </div>))}
+    ))}
+    </div>
     <p className="intro-description">
       {t('home.stacks2')}
     </p>
--- src/pages/data/stacksDocumentation.js
+++ src/pages/data/stacksDocumentation.js
@@ -1,22 +1,84 @@
-
 const stacksDocumentation = [
   {
-    git: 'https://git-scm.com/doc',
-    node: 'https://nodejs.org/en/docs/',
-    npm: 'https://docs.npmjs.com/',
-    mocha: 'https://mochajs.org/',
-    mysql: 'https://dev.mysql.com/doc/',
-    HTML: 'https://developer.mozilla.org/pt-BR/docs/Web/HTML',
-    CSS: 'https://developer.mozilla.org/pt-BR/docs/Web/CSS',
-    javascript: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript',
-    typescript: 'https://www.typescriptlang.org/docs/',
-    react: 'https://pt-br.reactjs.org/docs/getting-started.html',
-    redux: 'https://redux.js.org/',
-    jest: 'https://jestjs.io/pt-BR/docs/getting-started',
-    docker: 'https://docs.docker.com/',
+    documentation: 'https://git-scm.com/doc',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
+    alt: 'git-icon',
+    stack: "Git"
   },
-]
+  {
+    documentation: 'https://nodejs.org/en/docs/',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
+    alt: 'nodeJS-icon',
+    stack: "NodeJS"
+  },
+  {
+    documentation: 'https://docs.npmjs.com/',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg',
+    alt: 'npm-icon',
+    stack: "NPM"
+  },
+  {
+    documentation: 'https://mochajs.org/',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg',
+    alt: 'mocha-icon',
+    stack: "Mocha"
+  },
+  {
+    documentation: 'https://dev.mysql.com/doc/',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
+    alt: 'mysql-icon',
+    stack: "Mysql"
+  },
+  {
+    documentation: 'https://developer.mozilla.org/pt-BR/docs/Web/HTML',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+    alt: 'html-icon',
+    stack: "HTML"
+  },
+  {
+    documentation: 'https://developer.mozilla.org/pt-BR/docs/Web/CSS',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain-wordmark.svg',
+    alt: 'css-icon',
+    stack: "CSS"
+  },
+  {
+    documentation: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg',
+    alt: 'javascript-icon',
+    stack: "Javascript"
+  },
+  {
+    documentation: 'https://www.typescriptlang.org/docs/',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg',
+    alt: 'typescript-icon',
+    stack: "Typescript"
+  },
+  {
+    documentation: 'https://pt-br.reactjs.org/docs/getting-started.html',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    alt: 'react-icon',
+    stack: "React"
+  },
+  {
+    documentation: 'https://redux.js.org/',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+    alt: 'redux-icon',
+    stack: "Redux"
+  },
+  {
+    documentation: 'https://jestjs.io/pt-BR/docs/getting-started',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg',
+    alt: 'jest-icon',
+    stack: "Jest"
+  },
+  {
+    documentation: 'https://docs.docker.com/',
+    svg: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+    alt: 'docker-icon',
+    stack: "Docker"
+  },
+];
 
 module.exports = {
-  stacksDocumentation
-}
+  stacksDocumentation,
+};
